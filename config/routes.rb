67ff02b83c3d554d96491a8f7008fb30e842dcf7
Francesca Penaranda <penad@seas.upenn.edu>
2018-04-16 23:42:13 -0400

    made the controller, views and models and can make new restaurant/order/food/users

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :items
+  resources :orders
+  resources :users
+  resources :restaurants
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
